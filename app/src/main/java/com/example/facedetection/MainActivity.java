53bc1ba6fec45351282a26d453be91ac63e3b47c
abindran <abindranr@gmail.com>
2022-09-15 09:54:31 +0530

    The last stable version

--- app/src/main/java/com/example/facedetection/MainActivity.java
+++ app/src/main/java/com/example/facedetection/MainActivity.java
@@ -5,18 +5,28 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -27,6 +37,8 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
 
+import org.json.JSONArray;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -34,12 +46,21 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int GALLERY_REQUEST_CODE = 105;
     private Button cameraButton;
     private final static int REQUEST_IMAGE_CAPTURE  = 123;
     private ImageView imageView;
     private StorageReference mStorage;
+    private Button galleryButton;
+    private Button searchButton;
+    private RequestQueue requestQueue;
+
+    public static final String EXTRA_MESSAGE = "com.example.facedetection.MESSAGE";
+
     String currentPhotoPath;
     Uri downloadUrl;
+    String baseUrl = "https://e919-49-37-136-100.ngrok.io/";
+    String imageUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +70,54 @@ public class MainActivity extends AppCompatActivity {
         cameraButton = findViewById(R.id.openCamera);
         imageView = findViewById(R.id.imageView);
         mStorage = FirebaseStorage.getInstance().getReference();
+        galleryButton = findViewById(R.id.fromGallery);
+        searchButton = findViewById(R.id.search);
+        requestQueue =VolleySingleton.getInstance(this).getRequestQueue();
+
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dispatchTakePictureIntent();
+            }
+        });
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                gallery.setType("image/*");
+                startActivityForResult(gallery, GALLERY_REQUEST_CODE);
+            }
+        });
+
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myUrl = baseUrl + imageUrl;
+                Log.i("URL","URL: " + myUrl);
+                if(imageUrl != null) {
+                    sendApiRequest(myUrl);
+                }
+//                Intent intent = new Intent(MainActivity.this,DisplayMessageActivity.class);
+//                String message = "Hello from the before intent";
+//                intent.putExtra(EXTRA_MESSAGE,message);
+//                startActivity(intent);
+            }
+        });
+    }
+
+    private void sendApiRequest(String URL) {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, URL, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i("Response","Response: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Error","Error: " + error);
             }
         });
     }
@@ -66,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
             File f = new File(currentPhotoPath);
             imageView.setImageURI(Uri.fromFile(f));
-            Log.d("TAG","Absolute url: " + Uri.fromFile(f));
+            Log.d("TAG","Camera capture Absolute url: " + Uri.fromFile(f));
 
             Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
 
@@ -77,10 +141,33 @@ public class MainActivity extends AppCompatActivity {
             uploadImageToFirebase(f.getName(),contentUri);
         }
 
+        if(requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK){
+            Uri contentUri = data.getData();
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
+//            Log.d("TAG","Gallery Image Absolute url: " + imageFileName);
+            imageView.setImageURI(contentUri);
+            uploadImageToFirebase(imageFileName,contentUri);
+        }
+
+
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
     }
 
     private void uploadImageToFirebase(String name, Uri contentUri) {
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
         StorageReference filename = mStorage.child("Photos/" + name);
+
         UploadTask uploadTask =  filename.putFile(contentUri);
 
         Task<Uri>  urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
@@ -95,14 +182,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<Uri> task) {
                 if(task.isSuccessful()){
-                    Log.d("Checker: " , "Download Url is: " + task.getResult());
+//                    Log.d("Checker: " , "Download Url is: " + task.getResult());
                     downloadUrl = task.getResult();
+                    imageUrl = downloadUrl.toString();
+                    searchButton.setEnabled(true);
+                    progressDialog.dismiss();
                 }else{
                     Log.d("Error: ","Download Uri cannot be obtained");
                 }
             }
         });
     }
+
+
 
 
     private File createImageFile() throws IOException {
